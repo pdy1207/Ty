dc5669ea509729dbf0d6bdcfc934e5409615c84e
pdy1207 <pdyme1207@gmail.com>
2023-04-29 00:31:01 +0900

    Typescript

---
+++ index.js
@@ -0,0 +1,16 @@
+var 이름 = "kim";
+var 이름2 = ["kim", "park"];
+var 이름3 = { name: "kim" }; // name 속성은 옵션이에요
+var 나이 = "20";
+var 테스트 = 123;
+function 함수(x) {
+    return x * 2;
+}
+var john = [123, true];
+var add = { name: "kim" };
+var User = /** @class */ (function () {
+    function User(name) {
+        this.name = name;
+    }
+    return User;
+}());
