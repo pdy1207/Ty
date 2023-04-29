b7ba952df9d3f77556eebd0631e93c0d62074d57
pdy1207 <pdyme1207@gmail.com>
2023-04-29 23:15:13 +0900

    any / unknown |

--- index.ts
+++ index.ts
@@ -23,3 +23,36 @@ let project: { member: String[]; days: Number; started: Boolean } = {
   days: 30,
   started: true,
 };
+
+// == 문자 or 숫자 들어올 수 있는 변수는 ? 요러한걸 Union Type
+
+let 축구: Number | String = 123;
+
+let 배열: (Number | String)[] = [1, "2", 3];
+
+let 오브젝트: { a: string | Number } = { a: "123" };
+
+// any 타입 이건 쓰지마셈
+
+let phone: any;
+
+let 수학: String | Number;
+
+// ==
+
+let user: String = "kim";
+let age2: undefined | number = undefined;
+let married2: boolean = false;
+let 철수: (String | Boolean | undefined | number)[] = [user, age2, married2];
+
+let 학교: {
+  score: (number | Boolean)[];
+  teacher: String;
+  friend: String | String[];
+} = {
+  score: [100, 97, 84],
+  teacher: "Phil",
+  friend: "John",
+};
+학교.score[4] = false;
+학교.friend = ["Lee", 학교.teacher];
