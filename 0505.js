e5046991a511c2e9c314651ccf91b54d134b738b
pdy1207 <pdyme1207@gmail.com>
2023-05-06 23:57:48 +0900

    class / object 타입지정 interface 이것도있다

---
+++ 0505.js
@@ -0,0 +1,37 @@
+// var nunu = {
+//   q: "consume",
+//   w: "snowball",
+// };
+
+// var garen = {
+//   q: "strike",
+//   w: "courage",
+// };
+
+function 기계(a, b) {
+  this.q = a;
+  this.w = b;
+}
+기계.prototype.name = "kim";
+
+var nunu = new 기계("consume", "snowball");
+var garen = new 기계("strike", "courage");
+
+// ES 6 문법임
+
+class Hero {
+  constructor(a, b) {
+    this.q = a;
+    this.w = b;
+  }
+}
+
+// Q. array자료에 .sort() 붙일 수 있는 이유는?
+
+var 어레이 = [4, 2, 1];
+
+var 어레이 = new Array(4, 2, 1);
+어레이.sort();
+// Array.prototype <- 여기에 있기 때문에
+
+Array.prototype.함수 = function () {};
--- index.js
+++ index.js
@@ -1,37 +1,63 @@
-// var nunu = {
-//   q: "consume",
-//   w: "snowball",
-// };
-
-// var garen = {
-//   q: "strike",
-//   w: "courage",
-// };
-
-function 기계(a, b) {
-  this.q = a;
-  this.w = b;
-}
-기계.prototype.name = "kim";
-
-var nunu = new 기계("consume", "snowball");
-var garen = new 기계("strike", "courage");
-
-// ES 6 문법임
-
-class Hero {
-  constructor(a, b) {
-    this.q = a;
-    this.w = b;
-  }
-}
-
-// Q. array자료에 .sort() 붙일 수 있는 이유는?
-
-var 어레이 = [4, 2, 1];
-
-var 어레이 = new Array(4, 2, 1);
-어레이.sort();
-// Array.prototype <- 여기에 있기 때문에
-
-Array.prototype.함수 = function () {};
+var Person123 = /** @class */ (function () {
+    function Person123(a) {
+        this.name = a;
+    }
+    Person123.prototype.함수 = function (a) {
+        console.log("넌 몇살이니?" + a);
+    };
+    return Person123;
+}());
+var 사람1 = new Person123("qewr");
+// ==
+// class Test {
+//   name: string;
+//   constructor() {
+//     this.name = "kim";
+//   }
+// }
+var Test = /** @class */ (function () {
+    function Test(a) {
+        if (a === void 0) { a = "kim"; }
+        this.name = a;
+        this.age = 20;
+    }
+    return Test;
+}());
+var Test2 = /** @class */ (function () {
+    function Test2() {
+    }
+    Test2.prototype.add = function (숫자) {
+        console.log(숫자 + 1);
+    };
+    return Test2;
+}());
+var 네모 = {
+    color: "red",
+    width: 100,
+};
+var 학생 = {
+    name: "kim",
+};
+var 선생 = {
+    name: "kim",
+    age: 20,
+};
+var 상품 = {
+    brand: "Samsung",
+    serialNumber: 1360,
+    model: ["TV", "phone"],
+};
+var 장바구니 = [
+    { product: "청소기", price: 7000 },
+    { product: "삼다수", price: 800 },
+];
+var 장바구니2 = [{ product: "청소기", price: 7000, card: false }];
+var 오브젝트2 = {
+    plus: function (a, b) {
+        return a + b;
+    },
+    minus: function (a, b) {
+        return a - b;
+    },
+};
+// 함수타입은 ()=>{}
