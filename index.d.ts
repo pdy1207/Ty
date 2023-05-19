a869d722ac28c6c9193fa6ee734c72c8718bb825
pdy1207 <pdyme1207@gmail.com>
2023-05-19 21:20:41 +0900

    object 타입변환기 만들기

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,9 @@
-interface StringOnly {
-    [key: string]: string;
-}
-declare let user123: StringOnly;
-interface MyType123 {
-    "font-size": MyType123 | number;
-}
-declare let css: MyType123;
-interface objobj {
-    [key: string]: string | number;
-}
-declare let obj: objobj;
-interface obj2 {
-    [key: string]: number | obj2;
-    "font-size": number;
-}
-declare let obj2: obj2;
+type Benz = {
+    color: boolean;
+    model: boolean;
+    price: boolean | number;
+};
+type TypeChanger<MyType> = {
+    [key in keyof MyType]: string;
+};
+type 새로운타입 = TypeChanger<Benz>;
--- index.ts
+++ index.ts
@@ -1,67 +1,12 @@
-// interface StringOnly {
-//   name: string;
-//   age: string;
-//   location: string;
-// }
+type Benz = {
+  color: boolean;
+  model: boolean;
+  price: boolean | number;
+};
+// 전부다 string으로 바꾸려면?
 
-// let user123: StringOnly = {
-//   name: "kim",
-//   age: "20",
-//   location: "seoul",
-// };
-
-// 귀찮 ;;
-
-interface StringOnly {
-  [key: string]: string;
-}
-
-let user123: StringOnly = {
-  name: "kim",
-  age: "20",
-  location: "seoul",
+type TypeChanger<MyType> = {
+  [key in keyof MyType]: string;
 };
 
-interface MyType123 {
-  "font-size": MyType123 | number; // 안에 동일한게 많으면 이걸 사용 ㄱㄱ
-}
-
-let css: MyType123 = {
-  "font-size": {
-    "font-size": {
-      "font-size": 14,
-    },
-  },
-};
-
-// ===
-
-interface objobj {
-  [key: string]: string | number;
-}
-
-let obj: objobj = {
-  model: "k5",
-  brand: "kia",
-  price: 6000,
-  year: 2030,
-  date: "6월",
-  percent: "5%",
-  dealer: "김차장",
-};
-
-// ==
-interface obj2 {
-  [key: string]: number | obj2;
-  "font-size": number;
-}
-
-let obj2: obj2 = {
-  "font-size": 10,
-  secondary: {
-    "font-size": 12,
-    third: {
-      "font-size": 14,
-    },
-  },
-};
+type 새로운타입 = TypeChanger<Benz>;
